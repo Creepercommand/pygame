a4ec73b0905daa481573e30e5468beb6866b2e91
Creeper_command <creepercommand07@gmail.com>
2020-04-11 14:42:02 +0900

    창 크기조절

---
+++ pygame_00_ex2.py
@@ -0,0 +1,14 @@
+# 1024 x 600 크기의 창 만들기
+# 2020.04.11
+
+import time, pygame
+
+# 파이게임 초기화 및 생성하기
+width, heigh = 1024, 600
+win_size = (width,heigh)
+
+# 창 만들기
+screen = pygame.display.set_mode(win_size)
+
+#창 유지하기
+time.sleep(2)
