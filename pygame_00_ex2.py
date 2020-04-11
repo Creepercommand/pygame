d40f56743b530338a5bb8756ce9db734d16bfc9b
Creeper_command <creepercommand07@gmail.com>
2020-04-11 14:46:02 +0900

    창 만들기 2

--- pygame_00_ex2.py
+++ pygame_00_ex2.py
@@ -9,6 +9,7 @@ win_size = (width,heigh)
 
 # 창 만들기
 screen = pygame.display.set_mode(win_size)
-
+pygame.draw.rect(screen,(200,200,200), [10,10,10,50],2)
+pygame.display.filp()
 #창 유지하기
 time.sleep(2)
