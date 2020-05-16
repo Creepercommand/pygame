1df1bbf73ed8d1648d439f6ddac497f38ef75758
Creeper_command <creepercommand07@gmail.com>
2020-05-16 16:31:10 +0900

    스페이스 적 움직이기 주인공 움직이기

--- space02.py
+++ space02.py
@@ -1,11 +1,12 @@
 # 스페이스 인베이더 -02
 import pygame
 import os
+import  random
 
 WHITE = (255, 255, 255)
 SCREEN_WIDTH = SCREEN_HEIGHT = 750
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-
+clock = pygame.time.Clock()
 pygame.display.set_caption("SPACE INVADER")
 
 pygame.init()
@@ -16,6 +17,11 @@ bg_image = pygame.image.load(os.path.join("assets", "background-black.png"))
 bg_image = pygame.transform.scale(bg_image, SCREEN_SIZE)
 
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
 
 
 # 우주선 클래스 생성
@@ -29,7 +35,7 @@ class Ship:
         self.lazers = []
 
     def draw(self, window):
-        #pygame.draw.rect(window, (225, 0, 0), (self.x, self.y, 50, 50))
+        # pygame.draw.rect(window, (225, 0, 0), (self.x, self.y, 50, 50))
         window.blit(self.ship_img, (self.x, self.y))
 
     def get_width(self):
@@ -38,18 +44,34 @@ class Ship:
     def get_height(self):
         return self.ship_img.get_height()
 
+
 class Player(Ship):
-    def __init__(self, x, y, health = 100):
-        super().__init__(x,y,health)
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
         self.ship_img = YELLOW_SPACE_SHIP
         self.max_health = health
 
+class Enemy(Ship):
+    def __init__(self,x,y,health =100):
+        super().__init__(x,y,health)
+        self.ship_img = RED_SPACE_SHIP
+        self.health = health
+
+    def move(self, vel):
+        self.y += vel
+
+
 def main():
     run = True
+    FPS = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
 
+    #적군생성
+    enemies = []
+    wave_length = 5
+    enemy_vel = 5
     # 우주선
     player_vel = 5
     player = Player(300, 650)
@@ -62,12 +84,23 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (SCREEN_WIDTH - level_label.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
-        #WIN.blit(player.ship_img, (player.x, player.y))
+        # WIN.blit(player.ship_img, (player.x, player.y))
         pygame.display.update()
 
     while run:
+        clock.tick(FPS)
+
         redraw_window()
+
+        if len(enemies) == 0:
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randint(50,700),random.randint(-1000, -100))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -82,5 +115,8 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel < SCREEN_HEIGHT - player.get_height():
             player.y += 5
 
-
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+            if enemy.y > SCREEN_HEIGHT:
+                enemies.remove(enemy)
 main()
