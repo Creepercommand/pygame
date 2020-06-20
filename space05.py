b18743dc18ea9e7509cc9d489e478154c34ce2dc
Creeper_coding <creepercommand07@gmail.com>
2020-06-20 15:28:37 +0900

    스페이스 - 적 생성하기

--- space05.py
+++ space05.py
@@ -1,4 +1,4 @@
-'''
+"""
 스체이스 인베이더
 
 1.화면 설정 및 배경 이미지 출력
@@ -6,7 +6,7 @@
 3.적 생성하기
 4. 플레이어 레이저 발사
 5. 레이저 바라 속도 조정
-'''
+"""
 
 import pygame
 import os
@@ -31,10 +31,11 @@ bg_img = pygame.transform.scale(bg_img, SCREEN_SIZE)
 # 이미지 불러오기
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png'))
 
-#적군 이미지 불러오기
+# 적군 이미지 불러오기
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+
 
 # 비행선 클래스
 class Ship:
@@ -56,9 +57,25 @@ class Ship:
 
 # 아군 비행선 생성
 class Player(Ship):
-    def __init__(self,x,y,health=100):
-        super().__init__(x,y,health)
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
         self.ship_img = YELLOW_SPACE_SHIP
+
+
+# 적 비행기 생성
+class Enemy(Ship):
+    COLOR_MAP = {
+        'red': RED_SPACE_SHIP,
+        'blue': BLUE_SPACE_SHIP,
+        'green': GREEN_SPACE_SHIP
+    }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = self.COLOR_MAP[color]
+
+    def move(self, vel):
+        self.y += vel
 
 
 # 메인 함수
@@ -70,9 +87,12 @@ def main():
 
     clock = pygame.time.Clock()
 
-    player = Player(350,600)
+    player = Player(350, 600)
     player_vel = 5
 
+    enemies = []
+    wave_length = 5
+    enemy_vel = 2
 
     main_font = pygame.font.SysFont('comicsans', 50)
 
@@ -84,6 +104,9 @@ def main():
         WIN.blit(lives_lable, (10, 10))
         WIN.blit(level_lable, (SCREEN_WIDTH - level_lable.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
         pygame.display.update()
 
@@ -91,6 +114,14 @@ def main():
         clock.tick(FPS)
         redraw_window()
 
+        if len(enemies) == 0:
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randint(50, 700),
+                              random.randint(-1000, -100),
+                              random.choice(["red", "blue", "green"])
+                              )
+                enemies.append(enemy)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -98,13 +129,17 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a] and player.x - player_vel > 0:
             player.x -= player_vel
-        if keys[pygame.K_d] and player.x + player_vel  < SCREEN_WIDTH - player.get_width() :
+        if keys[pygame.K_d] and player.x + player_vel < SCREEN_WIDTH - player.get_width():
             player.x += player_vel
-        if keys[pygame.K_w] and player.y - player_vel > 0 :
+        if keys[pygame.K_w] and player.y - player_vel > 0:
             player.y -= player_vel
         if keys[pygame.K_s] and player.y + player_vel < SCREEN_HEIGHT - player.get_height():
             player.y += player_vel
 
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+            if enemy.y +enemy_vel > SCREEN_HEIGHT:
+                enemies.remove(enemy)
 
 
 main()
