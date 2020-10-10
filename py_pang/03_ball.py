5ee4b6ef1d5abaf81ab8c484850766ec38c45c9e
Creeper_coding <creepercommand07@gmail.com>
2020-10-10 16:02:31 +0900

    공 초기화

--- py_pang/03_ball.py
+++ py_pang/03_ball.py
@@ -41,6 +41,27 @@ weapon_speed = 10
 # 무기 저장소
 weapons = []
 
+#무기 저장소
+ball_images = [
+    pygame.image.load(os.path.join(img_path,"balloon1.png")),
+    pygame.image.load(os.path.join(img_path,"balloon2.png")),
+    pygame.image.load(os.path.join(img_path,"balloon3.png")),
+    pygame.image.load(os.path.join(img_path,"balloon4.png"))
+]
+bal_speed_y = [-18,-15,-12,-9]
+
+balls = []
+balls.append(
+    {
+        "pos_x" : 50,
+        "pos_y" : 50,
+        "img_idx" 0:,
+        "to_x" : 3,
+        "to_y" : -6,
+        "init_spd_y" : ball_speed_y[0]
+    }
+)
+
 # 게임 루프
 running = True
 while running:
@@ -55,9 +76,9 @@ while running:
             elif event.key == pygame.K_RIGHT:
                 character_speed = 5
             elif event.key == pygame.K_SPACE:
-                weapon_x_pos = character_x_pos + (character_width//2) - (weapon_width//2)
+                weapon_x_pos = character_x_pos + (character_width // 2) - (weapon_width // 2)
                 weapon_y_pos = character_y_pos
-                weapons.append([weapon_x_pos,weapon_y_pos])
+                weapons.append([weapon_x_pos, weapon_y_pos])
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 character_speed = 0
@@ -68,14 +89,14 @@ while running:
     if character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
 
-    #무기 위치 조정
-    weapons = [[w[0],w[1] - weapon_speed] for w in weapons]
+    # 무기 위치 조정
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
 
     weapons = [[w[0], w[1]] for w in weapons if w[1] > -screen_height]
 
     screen.blit(bg, (0, 0))
     for one in weapons:
-        screen.blit(weapon, (one[0],one[1]))
+        screen.blit(weapon, (one[0], one[1]))
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
 
