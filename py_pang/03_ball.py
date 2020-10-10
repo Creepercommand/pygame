9080884e2021dfe701e45e26a1c6ef5bed3cdd6c
Creeper_coding <creepercommand07@gmail.com>
2020-10-10 15:50:56 +0900

    무기 처리

--- py_pang/03_ball.py
+++ py_pang/03_ball.py
@@ -11,35 +11,74 @@ screen_size = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("PYPANG!")
 
+clock = pygame.time.Clock()
+
 # 배경이미지
 cu_path = os.path.dirname(__file__)
-img_path = os.path.join(cu_path,"image")
+img_path = os.path.join(cu_path, "image")
 bg = pygame.image.load(os.path.join(img_path, "background.png"))
 
-#바닥 이미지
-stage = pygame.image.load(os.path.join(img_path,"stage.png"))
+# 바닥 이미지
+stage = pygame.image.load(os.path.join(img_path, "stage.png"))
 stage_size = stage.get_rect().size
 stage_height = stage_size[1]
 
-#케릭터 이미지
-character = pygame.image.load(os.path.join(img_path,"character.png"))
+# 케릭터 이미지
+character = pygame.image.load(os.path.join(img_path, "character.png"))
 character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
-character_x_pos = screen_width //2 - character_width //2
-character_y_pos = screen_height - stage_height -character_height
+character_x_pos = screen_width // 2 - character_width // 2
+character_y_pos = screen_height - stage_height - character_height
+character_speed = 0
 
+# 무기 이미지
+weapon = pygame.image.load(os.path.join(img_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+weapon_speed = 10
+
+# 무기 저장소
+weapons = []
 
 # 게임 루프
 running = True
 while running:
+    clock.tick(120)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(bg, (0,0))
-    screen.blit(stage, (0,screen_height-stage_height))
-    screen.blit(character,(character_x_pos, character_y_pos))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_speed = -5
+            elif event.key == pygame.K_RIGHT:
+                character_speed = 5
+            elif event.key == pygame.K_SPACE:
+                weapon_x_pos = character_x_pos + (character_width//2) - (weapon_width//2)
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos,weapon_y_pos])
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_speed = 0
+
+    character_x_pos += character_speed
+    if character_x_pos < 0:
+        character_x_pos = 0
+    if character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    #무기 위치 조정
+    weapons = [[w[0],w[1] - weapon_speed] for w in weapons]
+
+    weapons = [[w[0], w[1]] for w in weapons if w[1] > -screen_height]
+
+    screen.blit(bg, (0, 0))
+    for one in weapons:
+        screen.blit(weapon, (one[0],one[1]))
+    screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
+
     pygame.display.update()
 
 # 게임종료
