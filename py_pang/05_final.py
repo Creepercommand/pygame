c6c146ef8f1f2a2f303b86cb3efd061c0dc78f32
Creeper_coding <creepercommand07@gmail.com>
2020-10-24 14:32:55 +0900

    3.캐릭터 이동

--- py_pang/05_final.py
+++ py_pang/05_final.py
@@ -8,6 +8,9 @@ import os
 # 파이게임 초기화
 pygame.init()
 
+# FPS
+clock = pygame.time.Clock()
+
 # 화면 설정
 screen_width = 640
 screen_height = 480
@@ -22,24 +25,45 @@ img_path = os.path.join(cur_path, "image")
 # 배경
 background_img = pygame.image.load(os.path.join(img_path, "background.png"))
 
-#무대
+# 무대
 stage_img = pygame.image.load(os.path.join(img_path, "stage.png"))
 stage_height = stage_img.get_rect().size[1]
 
-#캐릭터
+# 캐릭터
 character_img = pygame.image.load(os.path.join(img_path, "character.png"))
 character_rect = character_img.get_rect().size
 character_width = character_rect[0]
 character_height = character_rect[1]
-character_x_pos = screen_width // 2 - character_width//2
+character_x_pos = screen_width // 2 - character_width // 2
 character_y_pos = screen_height - stage_height - character_height
+character_speed = 0
 
 # 게임 루프
 running = True
 while running:
+    #FPS 처리
+    clock.tick(120)
+    # 이벤트 처리
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # 종료 처리
             running = False
+        # 키 처리
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                character_speed = 5
+            elif event.key == pygame.K_LEFT:
+                character_speed = -5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_speed = 0
+
+    # 캐릭터 움직임
+    character_x_pos += character_speed
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    if character_x_pos > screen_width- character_width:
+        character_x_pos = screen_width-character_width
 
     # 화면 출력
     screen.blit(background_img, (0, 0))
